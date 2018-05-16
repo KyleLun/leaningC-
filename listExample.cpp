f75a21523ae86deb3e90f5d022099b6cf4d3e06f
kyle <36875882+KyleLun@users.noreply.github.com>
2018-05-16 23:09:14 +0800

    list example

---
+++ listExample.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+	list <int> c;
+	list <int>::iterator c_iter;
+
+	c.push_back(1);
+	c.push_back(5);
+	c.push_back(2);
+	c.push_back(8);
+	c.push_back(-1);
+	c.push_back(9);
+	c.push_back(12);
+
+	cout << "ÅÅĞòÇ°: " << endl;
+	for (c_iter = c.begin(); c_iter != c.end(); c_iter ++) {
+		cout << *c_iter << " ";
+	}
+	cout << endl;
+
+	c.sort();
+
+	cout << "ÅÅĞòºó:" << endl;
+	for (c_iter = c.begin(); c_iter != c.end(); c_iter++) {
+		cout << *c_iter << " ";
+	}
+	cout << endl;
+
+	c.sort(greater<int>());
+
+	cout << "ÅÅĞòºó:" << endl;
+	for (c_iter = c.begin(); c_iter != c.end(); c_iter++) {
+		cout << *c_iter << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
